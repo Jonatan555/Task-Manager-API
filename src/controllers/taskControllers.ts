21227777a7b85df0d34bed838f3eefa63711a8e9
nathan lins <nathan.lins012@gmail.com>
2024-05-15 20:23:52 -0300

    add base project

---
+++ src/controllers/taskControllers.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const taskControllers = {
+  async read(_req: Request, res: Response, next: NextFunction) {
+    try {
+      return res.status(200).json({ message: "Tasks!" });
+    } catch (error) {
+      return next(error);
+    }
+  },
+};
---
+++ src/controllers/userControllers.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export const userControllers = {
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      return res.status(201).json("user created!");
+    } catch (error) {
+      return next(error);
+    }
+  },
+};
