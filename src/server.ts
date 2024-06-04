d5cd0ac257c0a07b77345193a8c36bd0c97ddc9e
nathan lins <nathan.lins012@gmail.com>
2024-06-04 18:32:23 -0300

    update project

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import "dotenv/config";
 import { routes } from "./routes";
-import { appErrors } from "./errors/appErrors";
-import { pageNotFound } from "./errors/pageNotFound";
 import { sqliteConnection } from "./databases/sqlite3";
 import { runMigrations } from "./databases/sqlite3/migrations";
-import cors from "cors";
+import { appErrors } from "./errors/appErrors";
+import { pageNotFound } from "./errors/pageNotFound";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +14,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-const whiteList = ["http://localhost.5173", "http://localhost.5173"];
+const whiteList = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  process.env.BASEURL_API,
+];
 
 app.use(
   cors({
@@ -22,6 +26,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(routes);
 
 app.use(pageNotFound);
@@ -33,6 +38,6 @@ app.listen(PORT, () => {
 
 sqliteConnection()
   .then(() => console.log("Database is connected..."))
-  .catch((error) => console.error("Database isn't connected -", error));
+  .catch((error) => console.error("Database is not connected - ", error));
 
 runMigrations();
